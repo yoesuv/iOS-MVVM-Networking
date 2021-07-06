9f9122b31bb85e9f9d7447846b332d66b0fd8917
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2021-07-06 16:41:00 +0700

    :pencil: setup splash screen

--- Simple App/ContentView.swift
+++ Simple App/ContentView.swift
@@ -9,13 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("Simple App")
+            .onTapGesture {
+                delay()
+            }
+    }
+    
+    func delay() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2, execute: {
+            print("Go to home after 2 seconds")
+        })
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+    
     static var previews: some View {
         ContentView()
     }
+    
 }
