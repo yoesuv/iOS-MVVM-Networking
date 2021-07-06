15651b6a4a27cac1cf06644dbe97ac44289f4ff7
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2021-07-06 16:19:54 +0700

    Initial Commit

---
+++ Simple App/Simple_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_AppApp.swift
+//  Simple App
+//
+//  Created by Yusuf Saifudin on 06/07/21.
+//
+
+import SwiftUI
+
+@main
+struct Simple_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
