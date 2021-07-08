866cc32e052630271e6e680325eea7bf3dd0b1c7
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2021-07-09 04:33:14 +0700

    :newspaper: add network manager

---
+++ Simple App/Networks/NetworkManager.swift
@@ -0,0 +1,36 @@
+//
+//  NetworkManager.swift
+//  Simple App
+//
+//  Created by Yusuf Saifudin on 09/07/21.
+//
+
+import Foundation
+
+class NetworkManager: ObservableObject {
+    
+    @Published var places = [Place]()
+    
+    func fetchPlaces() {
+        if let url = URL(string: "https://info-malang-batu.firebaseapp.com/List_place_malang_batu.json") {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if (error == nil) {
+                    let decoder = JSONDecoder()
+                    if let safeData = data {
+                        do {
+                            let results = try decoder.decode(Results.self, from: safeData)
+                            DispatchQueue.main.async {
+                                self.places = results.places
+                            }
+                        } catch {
+                            print(error)
+                        }
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+    
+}
