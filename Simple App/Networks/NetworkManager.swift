e6135617c11f11d46651093b5b350f6072620205
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2021-07-11 07:39:55 +0700

    :pencil: use alamofire for request

--- Simple App/Networks/NetworkManager.swift
+++ Simple App/Networks/NetworkManager.swift
@@ -6,30 +6,22 @@
 //
 
 import Foundation
+import Alamofire
 
 class NetworkManager: ObservableObject {
     
     @Published var places = [Place]()
     
     func fetchPlaces() {
-        if let url = URL(string: "https://info-malang-batu.firebaseapp.com/List_place_malang_batu.json") {
-            let session = URLSession(configuration: .default)
-            let task = session.dataTask(with: url) { (data, response, error) in
-                if (error == nil) {
-                    let decoder = JSONDecoder()
-                    if let safeData = data {
-                        do {
-                            let results = try decoder.decode([Place].self, from: safeData)
-                            DispatchQueue.main.async {
-                                self.places = results
-                            }
-                        } catch {
-                            print("NetworkManager # error \(error)")
-                        }
+        AF.request("https://info-malang-batu.firebaseapp.com/List_place_malang_batu.json", method: .get)
+            .responseDecodable(of: [Place].self) { (response) in
+                if (response.error == nil) {
+                    DispatchQueue.main.async {
+                        self.places = response.value ?? []
                     }
+                } else {
+                    print("NetworkManager # error \(response.error!)")
                 }
-            }
-            task.resume()
         }
     }
     
