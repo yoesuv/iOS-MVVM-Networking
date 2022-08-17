4aa284c76efe439ff14380a41e98782882122c09
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-08-17 08:41:04 +0700

    :pencil: add state loading

--- Simple App/ViewModels/HomeViewModel.swift
+++ Simple App/ViewModels/HomeViewModel.swift
@@ -10,11 +10,14 @@ import Foundation
 class HomeViewModel: ObservableObject {
     
     @Published var places = [Place]()
+    @Published var isLoading = true
     
     private let networkService = NetworkService()
     
     func fetchPlaces() {
+        isLoading = true
         networkService.fetchPlaces { response in
+            self.isLoading = false
             if (response.error == nil) {
                 self.places = response.value ?? []
             } else {
